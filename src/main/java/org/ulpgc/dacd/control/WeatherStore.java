55246f7675af362e1705f05e5661369e8679aeea
Tiopipi <dtc2004pelusa@gmail.com>
2023-11-07 17:02:43 +0000

    SqliteWeatherStore class and WeatherStore interface implemented

--- src/main/java/org/ulpgc/dacd/control/WeatherStore.java
+++ src/main/java/org/ulpgc/dacd/control/WeatherStore.java
@@ -1,4 +1,15 @@
 package org.ulpgc.dacd.control;
 
+import org.ulpgc.dacd.model.Location;
+import org.ulpgc.dacd.model.Weather;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Instant;
+
 public interface WeatherStore {
+     boolean save(java.sql.Statement statement, Weather weather) throws SQLException;
+     void createTable(Statement statement, Location location) throws SQLException;
+     boolean update(Statement statement, Weather weather) throws SQLException;
+     void dropTable(Statement statement) throws SQLException;
 }
