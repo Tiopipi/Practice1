c7cfe3d5add59611b77bad88a0ee41e009c98c9d
Tiopipi <dtc2004pelusa@gmail.com>
2023-11-12 09:49:38 +0000

    method getWeather modified on OpenWeatherMapSupplier

--- src/main/java/org/ulpgc/dacd/control/WeatherSupplier.java
+++ src/main/java/org/ulpgc/dacd/control/WeatherSupplier.java
@@ -2,13 +2,10 @@ package org.ulpgc.dacd.control;
 
 import org.ulpgc.dacd.model.Location;
 import org.ulpgc.dacd.model.Weather;
-
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 import java.time.Instant;
+import java.util.List;
 
 public interface WeatherSupplier {
-    Weather getWeather(Location location, Instant ts) throws IOException;
+    List<Weather> getWeather(Location location, List<Instant> ts, String apikey) throws IOException;
 }
