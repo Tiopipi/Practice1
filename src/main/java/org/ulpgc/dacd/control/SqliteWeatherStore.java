adf4f95df17cb9284a82e0eea26dbcbb74cf4bc5
Tiopipi <dtc2004pelusa@gmail.com>
2023-11-12 09:41:37 +0000

    dropTable method deleted and select method modified from SqliteWeatherStore

--- src/main/java/org/ulpgc/dacd/control/SqliteWeatherStore.java
+++ src/main/java/org/ulpgc/dacd/control/SqliteWeatherStore.java
@@ -2,11 +2,8 @@ package org.ulpgc.dacd.control;
 
 import org.ulpgc.dacd.model.Location;
 import org.ulpgc.dacd.model.Weather;
-
 import java.sql.*;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+
 
 public class SqliteWeatherStore implements WeatherStore {
     public boolean save(Statement statement, Weather weather) throws SQLException {
@@ -21,17 +18,18 @@ public class SqliteWeatherStore implements WeatherStore {
                 weather.getCloud() + ")";
         return statement.executeUpdate(insertQuery) > 0;
     }
+
     public void createTable(Statement statement, Location location) throws SQLException {
         statement.execute("CREATE TABLE IF NOT EXISTS " + location.getIsland() + " (" +
                 "date TEXT PRIMARY KEY,\n" +
                 "rainProbability REAL DEFAULT 0,\n" +
-                "wind REAL DEFAULT 0, \n"+
-                "temp REAL DEFAULT 0, \n"+
-                "humidity INT DEFAULT 0, \n"+
-                "cloud INT DEFAULT 0"+
-                ");");
+                "wind REAL DEFAULT 0, \n" +
+                "temp REAL DEFAULT 0, \n" +
+                "humidity INT DEFAULT 0, \n" +
+                "cloud INT DEFAULT 0);");
 
     }
+
     public boolean update(Statement statement, Weather weather) throws SQLException {
         String tableName = weather.getLocation().getIsland();
         String updateQuery = "UPDATE " + tableName + " SET " +
@@ -43,12 +41,10 @@ public class SqliteWeatherStore implements WeatherStore {
                 " WHERE date = '" + weather.getTs().toString() + "'"; // Asegúrate de que weather.getTs() devuelva una cadena adecuada
         return statement.executeUpdate(updateQuery) > 0;
     }
-    public void dropTable(Statement statement) throws SQLException {
-        statement.execute("DROP TABLE IF EXISTS GranCanaria;\n");
-    }
 
-    public ResultSet select(Statement statement, Weather weather) throws SQLException{
-        return statement.executeQuery("SELECT date FROM " +weather.getLocation().getIsland());
+    public ResultSet select(Statement statement, Weather weather) throws SQLException {
+        return statement.executeQuery("SELECT * FROM " + weather.getLocation().getIsland()
+                + " WHERE date = '" + weather.getTs().toString() + "'");
     }
 
 }
--- src/main/java/org/ulpgc/dacd/control/WeatherStore.java
+++ src/main/java/org/ulpgc/dacd/control/WeatherStore.java
@@ -2,16 +2,13 @@ package org.ulpgc.dacd.control;
 
 import org.ulpgc.dacd.model.Location;
 import org.ulpgc.dacd.model.Weather;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.Instant;
 
 public interface WeatherStore {
      boolean save(java.sql.Statement statement, Weather weather) throws SQLException;
      void createTable(Statement statement, Location location) throws SQLException;
      boolean update(Statement statement, Weather weather) throws SQLException;
      ResultSet select(Statement statement, Weather weather) throws  SQLException;
-     void dropTable(Statement statement) throws SQLException;
 }
